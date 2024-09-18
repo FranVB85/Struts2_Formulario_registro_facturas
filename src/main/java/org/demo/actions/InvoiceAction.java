32332666b6a09eda7f868c3d9c59290b6f09e4bf
Fran <fvasqueb@emeal.nttdata.com>
2024-09-18 17:40:03 +0200

    first commit

---
+++ src/main/java/org/demo/actions/InvoiceAction.java
@@ -0,0 +1,29 @@
+package org.demo.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.demo.actions.beans.InvoiceBean;
+
+public class InvoiceAction extends ActionSupport  {
+
+    InvoiceBean invoiceBean;
+
+    @Override
+    public String execute() throws Exception {
+        System.out.println("execute!!");
+        return SUCCESS;
+    }
+
+    public InvoiceBean getInvoiceBean() {
+        return invoiceBean;
+    }
+
+    public void setInvoiceBean(InvoiceBean invoiceBean) {
+        this.invoiceBean = invoiceBean;
+    }
+    @Override
+    public void validate() {
+        if (invoiceBean.getSubject().isEmpty()) {
+            addFieldError("invoiceBean.subject", "El concepto es obligatorio.");
+        }
+    }
+}
